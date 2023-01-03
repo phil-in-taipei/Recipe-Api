eb714ba8157fbd62102d1324b036f61cb422e3da
mikalikahi <sweeney.phil@gmail.com>
2023-01-03 18:35:31 +0800

    Testing practice for without jsonPath() and with along with a TestUtil class with helper methods

--- src/main/java/com/example/RecipeApi/RecipeApiApplication.java
+++ src/main/java/com/example/RecipeApi/RecipeApiApplication.java
@@ -2,8 +2,10 @@ package com.example.RecipeApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("test")
 public class RecipeApiApplication {
 
 	public static void main(String[] args) {
